6a150d47ea96eaf824cfba54ca15e62111770f03
Aishwarya Kulkarni <skay4kulkarni@gmail.com>
2019-12-19 22:02:47 +0530

    R2

---
+++ l2.R
@@ -0,0 +1,5 @@
+table(factor(c("Control", "Control", "Control", "Ear Removal", "Ear Removal", "Ear Removal", "Ear Removal", "Fake Ear Removal", "Fake Ear Removal", "Fake Ear Removal", "Fake Ear Removal", "Fake Ear Removal")))
+
+vector<-c(rep("a",25), rep("b",15), rep("c",58))
+length(vector)
+table(vector)
